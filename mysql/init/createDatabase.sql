bbcccf843a3f6279b045595838d7cdc9739cac2e
One Code Man <pedro.lucca@live.estacio.br>
2022-10-27 15:29:22 -0300

    new features

--- mysql/init/createDatabase.sql
+++ mysql/init/createDatabase.sql
@@ -1,4 +1,10 @@
-CREATE DATABASE IF NOT EXISTS `example_db` COLLATE 'utf8_general_ci' ;
-GRANT ALL ON `example_db`.* TO 'root'@'%' ;
+CREATE DATABASE IF NOT EXISTS `taskuro_db` COLLATE 'utf8_general_ci' ;
+GRANT ALL ON `taskuro_db`.* TO 'root'@'%' ;
+
+CREATE DATABASE IF NOT EXISTS `turnoverbnb_db` COLLATE 'utf8_general_ci' ;
+GRANT ALL ON `turnoverbnb_db`.* TO 'root'@'%' ;
+
+CREATE DATABASE IF NOT EXISTS `booking_db` COLLATE 'utf8_general_ci' ;
+GRANT ALL ON `booking_db`.* TO 'root'@'%' ;
 
 FLUSH PRIVILEGES ;
---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd ~/TidyDaily/docker/
+
+ECHO "*** STARTING BOOKING ***"
+gnome-terminal -x sh -c 'docker exec -it workspace_php8 sh -c "cd booking-api && php artisan consumers:work --topic=taskbird.booking.project.statuschanges"'
+
+
+ECHO "*** STARTING TASKBIRD ***"
+gnome-terminal -x sh -c 'docker exec -it workspace_php7 sh -c "cd taskuro-api && php artisan consumers:work --topic=booking.orders.confirmed"'
+gnome-terminal -x sh -c 'docker exec -it workspace_php7 sh -c "cd taskuro-api && php artisan queue:listen --queue=medium"'
+
+
+
+#gnome-terminal -x sh -c 'docker exec -it workspace_php7 sh -c "cd taskuro-api && php artisan migrate:fresh --seed && php artisan passport:install"'
